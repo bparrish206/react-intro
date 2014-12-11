530e5768081f0f706bc1387fb608438d5314fa37
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-10 18:22:02 -0800

    setup

---
+++ app/js/app.jsx
@@ -0,0 +1,21 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+window.$ = require('jquery');
+window._ = require('lodash');
+var React = require('react/addons');
+window.React = React;
+
+var Avatar = React.createClass ({
+render: function() {
+return ( <div className="Avatar"> {this.props.name}</div>)
+}
+});
+
+React.render(<Avatar name ="Brent"/>, document.getElementById('mount-point'));
+
+console.log('font-end app initialized');
+var app = {foo: 'bar'};
+
+module.exports = app;
